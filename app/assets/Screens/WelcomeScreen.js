8ccaadf8f2beb262869ee28f83b44cc430a2724e
Saafi Rahman <saafirahman17217@gmail.com>
2023-06-10 15:27:34 -0700

    added to the base template of the welcome page

--- app/assets/Screens/WelcomeScreen.js
+++ app/assets/Screens/WelcomeScreen.js
@@ -7,7 +7,7 @@ function WelcomeScreen(props) {
             <View style={styles.container}>
               <ImageBackground source={{
                 uri: 'https://m.media-amazon.com/images/M/MV5BODcwNWE3OTMtMDc3MS00NDFjLWE1OTAtNDU3NjgxODMxY2UyXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg'}} style={styles.background}>
-
+                <Image style={styles.logo} source={require("../../assets/mlady.jpg")} />
                 <View style={styles.loginpanel}/>
                 <View style={styles.registerpanel}/>
               </ImageBackground>
@@ -21,7 +21,8 @@ const styles = StyleSheet.create({
     },
     background: {
       flex: 1,
-      justifyContent: 'flex-end'
+      justifyContent: 'flex-end',
+      alignItems: "center"
     },
   
     loginpanel: {
@@ -34,6 +35,13 @@ const styles = StyleSheet.create({
         width: "100%",
         height: 70,
         backgroundColor: 'orange'
+      },
+
+      logo: {
+        width: 100,
+        height: 100,
+        position: 'absolute',
+        top: 100
       }
   });
 
