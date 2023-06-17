10c6ba972737f71c65ae65020da22e19df5a0721
SaafiRahman <saafirah@student.ubc.ca>
2023-06-16 21:38:34 -0700

    added some startercode for the homepage

---
+++ app/assets/Screens/HomePageScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function HomePageScreen(props) {
+    return (
+        <View >
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
+export default HomePageScreen;
